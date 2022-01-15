17ff372cc8f56c50dba233b7533dc1008fc7e4cb
JabJ <sajjadjafaribojd@gmail.com>
2022-01-15 12:33:16 +0330

    Update main_functional.py

--- main_functional.py
+++ main_functional.py
@@ -1,6 +1,7 @@
 import random
 
 def generate_phone_number(final,Prefix_number,num_arr):
+    phonebook_arr=[]
     for total in range(0,500):
         for num in range(0,7):
             n=random.randint(0,9)
@@ -9,12 +10,12 @@ def generate_phone_number(final,Prefix_number,num_arr):
         if num == 6:
             for j in num_arr:
                 final=final+str(j)  
-            print (random.choice(Prefix_number)+final+"\n")    
+            phonebook_arr.append(random.choice(Prefix_number)+final)
             num_arr.clear()
             final=""
             
+    return phonebook_arr
 
-
-generate_phone_number(final="",Prefix_number=["0915","0912","0910"],num_arr=[])
-
-   
+phonebook= generate_phone_number(final="",Prefix_number=["0915","0912","0910"],num_arr=[])
+for phone_number in phonebook:
+    print (phone_number)
